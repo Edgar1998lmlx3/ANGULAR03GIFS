bd4c511c23e77fd9b66fd46f3fdbcce2480eeae6
Edgar Campa <edgar-efra@hotmail.com>
2024-02-28 23:53:34 -0600

    :d cambios nocturnos

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifCardComponent } from './components/gif-card/gif-card.component';
 
 
 
@@ -11,6 +12,7 @@ import { CardListComponent } from './components/card-list/card-list.component';
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
+    GifCardComponent,
   ],
   imports: [
     CommonModule
