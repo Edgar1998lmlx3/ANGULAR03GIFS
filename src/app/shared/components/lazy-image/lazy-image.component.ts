49552723d79c6b8384375c5f86371236fdecacab
Edgar1998lmlx3 <edgar-efra@hotmail.com>
2024-02-29 15:32:35 -0600

    casi por finalizar

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
+})
+export class LazyImageComponent implements OnInit {
+
+
+  @Input()
+  public url!: string;
+
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+   if (!this.url) throw new Error('Url Property is required');
+  }
+
+  onLoad(){
+    console.log('image loaded');
+    this.hasLoaded = true;
+  }
+}
